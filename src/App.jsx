a8434292d779d4d72e7653b27c73e134ee164527
A-DSL <alicecemails@gmail.com>
2021-07-16 09:32:28 +0100

    Added imports, removed default styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { useState, useEffect } from "react";
 
+//I have no idea what this statement means so I'm just going to assume it's the same thing as defining App
 export default class App extends Component {
   render() {
     return <h1>Let's drink beer!</h1>;
