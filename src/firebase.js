57eccad378faa4313f028ac37c4777e25339190a
A-DSL <alicecemails@gmail.com>
2021-08-26 16:35:27 +0100

    Reworked firebase.

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4w3eNT7YY_HUnitfbl1yIA9xr4lNB6y0",
+  authDomain: "punk-api-challenge-3a168.firebaseapp.com",
+  projectId: "punk-api-challenge-3a168",
+  storageBucket: "punk-api-challenge-3a168.appspot.com",
+  messagingSenderId: "861090096469",
+  appId: "1:861090096469:web:296393a43e57a4fa35bbdc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
